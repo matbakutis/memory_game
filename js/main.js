53ec58877015da24e817d515389bb4bc4a981ff2
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-09-03 15:08:15 -0500

    Switched Array to Object

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 // Javascript for Memory Game
 // Matas Bakutis
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+		{
+			rank: "queen",
+			suit: "hearts",
+			cardImage: "images/queen-of-hearts.png"
+		},
+		{
+			rank: "queen",
+			suit: "diamonds",
+			cardImage: "images/queen-of-diamonds.png"			
+		},
+		{
+			rank: "king",
+			suit: "hearts",
+			cardImage: "images/king-of-hearts.png"
+		},
+		{
+			rank: "king",
+			suit: "diamonds",
+			cardImage: "images/king-of-diamonds.png"			
+		}
+			];
 var cardsInPlay = [];
 
 var checkForMath = function(){
@@ -17,9 +38,11 @@ var checkForMath = function(){
 
 var flipCard = function(cardId){
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMath();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
 
 flipCard(0);
